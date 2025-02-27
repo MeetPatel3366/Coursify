d7c25f3a75314901be1cb18676388697a1fa586c
Meet Patel <meet3366patel@gmail.com>
2025-02-27 19:49:38 +0530

    added react hot toast

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,8 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Component imports
+import App from "./App.jsx";
+// CSS imports
+import "./index.css";
+// Library imports
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-)
+    <Toaster />
+  </BrowserRouter>
+);
