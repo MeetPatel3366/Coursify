7aebe77b99372e91889fbf273ce8524e58411025
Meet Patel <meet3366patel@gmail.com>
2025-03-02 17:59:14 +0530

    Add Login ,Signup,Login,Logout Buttons on Slidebar

--- Client/src/Layouts/HomeLayout.jsx
+++ Client/src/Layouts/HomeLayout.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import { FiMenu } from "react-icons/fi";
 import { AiFillCloseCircle } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Footer from "../Components/Footer";
 
 const HomeLayout = ({ children }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  //for checking if user is logged in
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+  //for displaying the options according to role
+  const role = useSelector((state) => state?.auth?.role);
+
   const handleChangeWidth = () => {
     const drawerSide = document.getElementsByClassName("drawer-side");
-    drawerSide[0].style.width = 'auto';
+    drawerSide[0].style.width = "auto";
   };
-  const handleHideDrawer=()=>{
-    const element=document.getElementsByClassName("drawer-toggle");
-    element[0].checked=false;
+  const handleHideDrawer = () => {
+    const element = document.getElementsByClassName("drawer-toggle");
+    element[0].checked = false;
 
     const drawerSide = document.getElementsByClassName("drawer-side");
-    drawerSide[0].style.width = '0';
-  }
+    drawerSide[0].style.width = "0";
+  };
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+
+    // const res=await dispatch(logout())
+
+    // if(res?.payload?.success)
+    navigate("/");
+  };
   return (
     <div className="min-h-[90vh]">
       <div className="drawer absolute left-0 z-50 w-fit">
@@ -39,11 +58,20 @@ const HomeLayout = ({ children }) => {
           <ul className="menu w-48 p-4 sm:w-80 bg-base-200 text-base-content relative">
             {/* Sidebar content here */}
             <li className="w-fit absolute right-2 z-50">
-              <button><AiFillCloseCircle onClick={handleHideDrawer} size={24}/></button>
+              <button>
+                <AiFillCloseCircle onClick={handleHideDrawer} size={24} />
+              </button>
             </li>
             <li>
               <Link to="/">Home</Link>
             </li>
+
+            {isLoggedIn && role == "ADMIN" && (
+              <li>
+                <Link to="/admin/dashboard">Admin DashBoard</Link>
+              </li>
+            )}
+
             <li>
               <Link to="/courses">All Courses</Link>
             </li>
@@ -53,13 +81,41 @@ const HomeLayout = ({ children }) => {
             <li>
               <Link to="/about">About Us</Link>
             </li>
+
+            {!isLoggedIn && (
+              <li className="absolute bottom-0 w-[90%]">
+                <div className="w-full flex items-center justify-center">
+                  <button className="px-4 py-1 font-semibold rounded-md">
+                    <Link to="/login">Login</Link>
+                  </button>
+                  <button className=" px-4 py-1 font-semibold rounded-md">
+                    <Link to="/login">Signup</Link>
+                  </button>
+                </div>
+              </li>
+            )}
+
+            {isLoggedIn && (
+              <li className="absolute bottom-0 w-[90%]">
+                <div className="w-full flex items-center justify-center">
+                  <button className="px-4 py-1 font-semibold rounded-md">
+                    <Link to="/user/profile">profile</Link>
+                  </button>
+                  <button className=" px-4 py-1 font-semibold rounded-md">
+                    <Link onClick={handleLogout} to="/logout">
+                      Logout
+                    </Link>
+                  </button>
+                </div>
+              </li>
+            )}
           </ul>
         </div>
       </div>
 
       {children}
 
-      <Footer/>
+      <Footer />
     </div>
   );
 };
