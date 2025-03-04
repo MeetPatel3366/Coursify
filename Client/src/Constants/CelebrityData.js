f5eab1fac2adb1bebbd8dc6d8a54350dc6c8bd9d
Meet Patel <meet3366patel@gmail.com>
2025-03-04 20:45:48 +0530

    Cleaned up about up

---
+++ Client/src/Constants/CelebrityData.js
@@ -0,0 +1,42 @@
+import apj from "../assets/images/apj.jpg";
+import billGates from "../assets/images/billGates.jpg";
+import einstein from "../assets/images/einstein.jpg";
+import nelsonMandela from "../assets/images/nelsonMandela.jpg";
+import steveJobs from "../assets/images/steveJobs.jpg";
+
+export const celebrities = [
+  {
+    image: apj,
+    title: "Dr. A. P. J. Abdul Kalam",
+    description: "Education is the key to unlock the golden door of freedom.",
+    slideNumber: 1,
+  },
+  {
+    image: steveJobs,
+    title: "Steve Jobs",
+    description:
+      "What's wrong with education cannot be fixed with technology. No amount of technology will make a dent. The problems are sociopolitical.",
+    slideNumber: 2,
+  },
+  {
+    image: nelsonMandela,
+    title: "Nelson Mandela",
+    description:
+      "Education is the most powerful weapon which you can use to change the world.",
+    slideNumber: 3,
+  },
+  {
+    image: einstein,
+    title: "Albert Einstein",
+    description:
+      "Education is not the learning of facts, but the training of the mind to think.",
+    slideNumber: 4,
+  },
+  {
+    image: billGates,
+    title: "Bill Gates",
+    description:
+      "Everyone needs a coach. It doesn't matter whether you're a basketball player, a tennis player, a gymnast, or a bridge player.",
+    slideNumber: 5,
+  },
+];
--- Client/src/Pages/AboutUs.jsx
+++ Client/src/Pages/AboutUs.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import HomeLayout from "../Layouts/HomeLayout";
 import aboutImg from "../assets/images/about.png";
-import apj from "../assets/images/apj.jpg";
-import billGates from "../assets/images/billGates.jpg";
-import einstein from "../assets/images/einstein.jpg";
-import nelsonMandela from "../assets/images/nelsonMandela.jpg";
-import steveJobs from "../assets/images/steveJobs.jpg";
+import CarouselSlide from "../Components/CarouselSlide";
+import { celebrities } from "../Constants/CelebrityData";
 
 const AboutUs = () => {
   return (
@@ -37,114 +34,16 @@ const AboutUs = () => {
         </div>
 
         <div className="carousel w-1/2 m-auto my-48">
-          <div id="slide1" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={apj}
-                className="w-40 rounded-full border-2 border-gray-400"
-              />
-              <p className="text-xl text-gray-200">
-                "Education is the key to unlock the golden door of freedom."
-              </p>
-              <h3 className="text-2xl font-semibold">
-                Dr. A. P. J. Abdul Kalam
-              </h3>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide5" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide2" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide2" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={steveJobs}
-                className="w-40 rounded-full border-2 border-gray-400"
-              />
-              <p className="text-xl text-gray-200">
-                "What's wrong with education cannot be fixed with technology. No
-                amount of technology will make a dent. The problems are
-                sociopolitical."
-              </p>
-              <h3 className="text-2xl font-semibold">Steve Jobs</h3>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide1" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide3" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide3" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={nelsonMandela}
-                className="w-40 rounded-full border-2 border-gray-400"
-              />
-              <p className="text-xl text-gray-200">
-                "Education is the most powerful weapon which you can use to
-                change the world."
-              </p>
-              <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide2" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide4" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide4" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={einstein}
-                className="w-40 rounded-full border-2 border-gray-400"
-              />
-              <p className="text-xl text-gray-200">
-                "Education is not the learning of facts, but the training of the
-                mind to think."
-              </p>
-              <h3 className="text-2xl font-semibold">Albert Einstein</h3>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide3" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide5" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide5" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={billGates}
-                className="w-40 rounded-full border-2 border-gray-400"
-              />
-              <p className="text-xl text-gray-200">
-                "Everyone needs a coach. It doesn't matter whether you're a
-                basketball player, a tennis player, a gymnast, or a bridge
-                player."
-              </p>
-              <h3 className="text-2xl font-semibold">Bill Gates</h3>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide4" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide1" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
+          {celebrities &&
+            celebrities.map((curCelebrity) => {
+              return (
+                <CarouselSlide
+                  {...curCelebrity}
+                  key={curCelebrity.slideNumber}
+                  totalSlides={celebrities.length}
+                />
+              );
+            })}
         </div>
       </div>
     </HomeLayout>
