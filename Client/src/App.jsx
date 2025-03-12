f5eca535fc4ae285622e128bcbcf6e4c3c2a1660
Meet Patel <meet3366patel@gmail.com>
2025-03-12 22:08:17 +0530

    implement course list page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,6 +5,7 @@ import AboutUs from "./Pages/AboutUs";
 import NotFound from "./Pages/NotFound";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
+import CourseList from "./Pages/Course/CourseList";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/about" element={<AboutUs />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/courses" element={<CourseList />} />
 
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
