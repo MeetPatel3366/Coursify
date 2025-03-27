0a0f518c87cc7c96146f9ea34013a76a282b32b7
Meet Patel <meet3366patel@gmail.com>
2025-03-27 18:28:16 +0530

    Implement user statistics retrieval for admin dashboard

--- Server/controllers/other.controller.js
+++ Server/controllers/other.controller.js
@@ -1,3 +1,4 @@
+import User from "../models/user.model.js";
 import AppError from "../utils/error.util.js";
 import sendEmail from "../utils/sendEmail.js";
 
@@ -28,4 +29,19 @@ const contactUs = async (req, res, next) => {
   }
 };
 
-export { contactUs };
+const userStats = async (req, res, next) => {
+  const allUsersCount = await User.countDocuments();
+
+  const subscribedUsersCount = await User.countDocuments({
+    "subscription.status": "active",
+  });
+
+  req.status(200).json({
+    success: true,
+    message: "All registered users count",
+    allUsersCount,
+    subscribedUsersCount,
+  });
+};
+
+export { contactUs, userStats };
--- Server/routes/other.routes.js
+++ Server/routes/other.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { contactUs } from "../controllers/other.controller.js";
+import { contactUs, userStats } from "../controllers/other.controller.js";
+import { authorizedRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/contact", contactUs);
+
+router.get(
+  "/admin/stats/users",
+  isLoggedIn,
+  authorizedRoles("ADMIN"),
+  userStats
+);
 
 export default router;
